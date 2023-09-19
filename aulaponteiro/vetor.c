88d99cbb014752d03fe9173b2fed3e667d8ac76a
jhvlima <joao.h.lima@edu.ufes.br>
2023-09-19 14:58:42 -0300

    vetor

--- aulaponteiro/vetor.c
+++ aulaponteiro/vetor.c
@@ -4,6 +4,6 @@ int main(){
     float notas[10] = {1.2, 7.7, 4.9, 10, 6.5, 7.1, 8.2, 9.1, 8.9, 7.0};
     float *pont;
     pont = notas;
-    printf("- notas = %f\n", *pont);
+    printf("- notas = %f\n", (*pont)+3);
     return 0;
 }
